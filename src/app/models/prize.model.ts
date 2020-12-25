d1057d49ef3461730e5e990a2ce4a64645ac4103
zhan <grianchan@gmail.com>
2020-12-25 18:15:36 +0800

    change canvas lib

--- src/app/models/prize.model.ts
+++ src/app/models/prize.model.ts
@@ -1,4 +1,7 @@
-export interface Prize {
-  title: string;
-  discover: boolean;
+import ClearPicture from 'clear-picture';
+
+export class Prize {
+  public title = '';
+  public discover = false;
+  public clearPicture?: ClearPicture;
 }
--- src/app/views/scratch/scratch.component.ts
+++ src/app/views/scratch/scratch.component.ts
@@ -1,14 +1,14 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {Prize} from '../../models/prize.model';
 import {Router} from '@angular/router';
-import {ScratchCard} from '../../lib/scratchcard';
+import ClearPicture from 'clear-picture';
 
 @Component({
   selector: 'app-scratch',
   templateUrl: './scratch.component.html',
   styleUrls: ['./scratch.component.sass']
 })
-export class ScratchComponent implements OnInit {
+export class ScratchComponent implements OnInit, AfterViewInit {
 
   public prizes: Prize[] = [
     {
@@ -44,12 +44,30 @@ export class ScratchComponent implements OnInit {
 
   ngOnInit(): void {
     this.randomPrizes();
-    const canvas = document.getElementById('canvas') as HTMLCanvasElement | null;
-    if (canvas === null) {
-      return;
-    }
-    const card = new ScratchCard(canvas);
-    card.start();
+
+  }
+
+  ngAfterViewInit(): void {
+    this.prizes.map((prize: Prize, i) => {
+      const card = document.getElementById('card-' + i);
+      if (card === null) {
+        return;
+      }
+      console.log(card.style.height);
+      prize.clearPicture = new ClearPicture({
+        canvas: document.getElementById('canvas-' + i), // 页面中的 canvas 元素
+        background: '../assets/images/fightting.png', // 用于涂抹的背景，涂抹完成后消失
+        canvasWidth: card.style.width, // canvas 宽度
+        canvasHeight: card.style.height, // canvas 高度
+        lineWidth: 60, // 画笔粗细
+        completeRatio: 0.5, // 完成度，最大值为 1
+        finishCallback: () => {
+          prize.discover = true;
+          console.log('end');
+          // 完成回调
+        },
+      });
+    });
   }
 
   private randomPrizes(): void {
@@ -83,5 +101,13 @@ export class ScratchComponent implements OnInit {
 
   public setting(): void {
     this.router.navigate(['/setting']);
+  }
+
+  getOpacity(prize: Prize): number {
+    if (prize.discover) {
+      return 1;
+    } else {
+      return 0;
+    }
   }
 }
